e6a411b1f69d6f683b084814cd605042ce0b53f7
Purvesh tejani <tejani.purvesh7493@icloud.com>
2019-12-11 16:49:34 +0530

    imageCropping Completed

--- cropping/ViewController.swift
+++ cropping/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UIScrollViewDelegate {
+    
+    @IBOutlet weak var imgview: UIImageView!
+    
+    @IBOutlet weak var scrView: UIScrollView!
+    
+    @IBOutlet weak var imgheight: NSLayoutConstraint!
+    
+    @IBOutlet weak var imgwidth: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imgview.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.imgtap)))
+        imgview.isUserInteractionEnabled = true
     }
-
-
+    
+    @objc func imgtap ()
+    {
+        let picker = UIImagePickerController();
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+         picker.allowsEditing = true
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.editedImage] as? UIImage
+        {
+            imgview.image = image
+        }
+        
+        else if let image = info[.originalImage] as? UIImage
+        {
+            imgview.image = image
+        }
+    
+        self.dismiss(animated: true) {
+            
+            
+        }
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        
+        return imgview;
+    }
+    
+    
+   
 }
 
